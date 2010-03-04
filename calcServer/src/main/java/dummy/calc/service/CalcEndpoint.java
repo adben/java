bd9ce2fe49d29ae967c0a20cddf7d31dc9ad228b
Adolfo Benedetti <adolfo.benedetti@gmail.com>
2010-03-04 23:55:39 +0100

    service => operation

--- calcServer/src/main/java/dummy/calc/service/CalcEndpoint.java
+++ calcServer/src/main/java/dummy/calc/service/CalcEndpoint.java
@@ -14,44 +14,45 @@ import dummy.calc.bean.GenericResponse;
 @Endpoint
 public class CalcEndpoint {
 
-    private CalcService calcService;
-    protected final Log logger = LogFactory.getLog(getClass());
+	private CalcService calcService;
+	protected final Log logger = LogFactory.getLog(getClass());
 
-    /**
-     * Sets the "business service" to delegate to.
-     */
-    public void setCalcService(CalcService calcService) {
-        this.calcService = calcService;
-    }
+	/**
+	 * Sets the "business service" to delegate to.
+	 */
+	public void setCalcService(CalcService calcService) {
+		this.calcService = calcService;
+	}
 
-    /**
-     * Process the "multiply" operation.
-     */
-    @PayloadRoot(localPart = "multiply-request", namespace = "http://dummy/calc")
-    public GenericResponse doMultiply(MultiplyRequest request) {
-    	String operation="multiply";
-        GenericResponse response = new GenericResponse();
-                //fill response
-        response.setTerm1(request.getTerm1());
-        response.setTerm2(request.getTerm2());
-        response.setOperation(operation);
-        response.setResponse(calcService.processNumberTypeOperation(request.getTerm1(), request.getTerm2(), operation));
-        return response;
-    }
+	/**
+	 * Process the "multiply" operation.
+	 */
+	@PayloadRoot(localPart = "multiply-request", namespace = "http://dummy/calc")
+	public GenericResponse doMultiply(MultiplyRequest request) {
+		String operation = "multiply";
+		GenericResponse response = new GenericResponse();
+		// fill response
+		response.setTerm1(request.getTerm1());
+		response.setTerm2(request.getTerm2());
+		response.setOperation(operation);
+		response.setResponse(calcService.processNumberTypeOperation(request
+				.getTerm1(), request.getTerm2(), operation));
+		return response;
+	}
 
-     /**
-     * Process the "divide" operation.
-     */
-    @PayloadRoot(localPart = "divide-request", namespace = "http://dummy/calc")
-    public GenericResponse doDivide(DivideRequest request) {
-    	String operation="divide";
-        GenericResponse response = new GenericResponse();
-        response.setTerm1(request.getTerm1());
-        response.setTerm2(request.getTerm2());
-        response.setOperation(operation);
-        response.setResponse(calcService.processNumberTypeOperation(request.getTerm1(), request.getTerm2(), operation));
-        return response;
-    }
-
+	/**
+	 * Process the "divide" operation.
+	 */
+	@PayloadRoot(localPart = "divide-request", namespace = "http://dummy/calc")
+	public GenericResponse doDivide(DivideRequest request) {
+		String operation = "divide";
+		GenericResponse response = new GenericResponse();
+		response.setTerm1(request.getTerm1());
+		response.setTerm2(request.getTerm2());
+		response.setOperation(operation);
+		response.setResponse(calcService.processNumberTypeOperation(request
+				.getTerm1(), request.getTerm2(), operation));
+		return response;
+	}
 
 }
